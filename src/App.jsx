3c03f6077931f5564819f459ae5b652891f121a4
Faith-254-UI <nivafaith@gmail.com>
2025-10-13 11:45:14 +0300

    Fix file casing and update Menu and Checkout components

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Cart from "./components/Cart";
-import Checkout from "./pages/Checkout";
+import Checkout from "./pages/checkout";
 import Menu from "./pages/Menu";
 
 function App() {
